db0f2106a77ea3d293cf89a094ca5d4de3043f6c
Scott Ritchie <sritchie73@gmail.com>
2023-06-19 15:03:42 +0100

    Restarting project

--- src/utils/factor_by_size.R
+++
@@ -1,8 +0,0 @@
-library(data.table)
-
-# Function for converting a set of numbers to a factor, ordering levels by group size
-factor_by_size <- function(x) {
-  group_sizes <- as.data.table(table(x))
-  group_sizes <- group_sizes[order(-N)]
-  factor(x, levels=group_sizes$x)
-}
