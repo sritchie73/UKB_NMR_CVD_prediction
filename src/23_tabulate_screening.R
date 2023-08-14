fd1504fbbcf4a242f4e8a624c0b27b08f1fe4087
Scott Ritchie <sritchie73@gmail.com>
2023-08-14 12:25:29 +0100

    output tables for supp

--- src/23_tabulate_screening.R
+++ src/23_tabulate_screening.R
@@ -105,3 +105,100 @@ dat <- dat[order(model)][order(strategy)]
 # Write out
 fwrite(dat, sep="\t", quote=FALSE, file="analyses/public_health_modelling/screening_summary.txt")
 
+# Build supp tables
+dt1 <- dcast(blanket, sex + people + cases + model ~ number, value.var=c("estimate", "L95", "U95"))
+dt1[, sex := factor(sex, levels=c("Males", "Females"))]
+dt1[, model := factor(model, levels=c("SCORE2", "SCORE2 + NMR scores", "SCORE2 + PRSs", "SCORE2 + NMR scores + PRSs"))]
+dt1 <- dt1[order(model)][order(sex)]
+dt1 <- dt1[,.(sex, people, cases, model, 
+  estimate_high_risk, L95_high_risk, U95_high_risk,
+  estimate_high_risk_cases, L95_high_risk_cases, U95_high_risk_cases,
+  estimate_events_prevented, L95_events_prevented, U95_events_prevented,
+  estimate_NNS, L95_NNS, U95_NNS,
+  estimate_NNT, L95_NNT, U95_NNT
+)]
+fwrite(dt1, sep="\t", quote=FALSE, file="analyses/public_health_modelling/blanket_screening_supp.txt")
+
+dt2 <- dcast(targeted, sex + people + cases + model ~ number, value.var=c("estimate", "L95", "U95"))
+dt2[, sex := factor(sex, levels=c("Males", "Females"))]
+dt2[, model := factor(model, levels=c("SCORE2", "SCORE2 + NMR scores", "SCORE2 + PRSs", "SCORE2 + NMR scores + PRSs"))]
+dt2 <- dt2[order(model)][order(sex)]
+dt2 <- dt2[,.(sex, people, cases, model, 
+  estimate_high_risk, L95_high_risk, U95_high_risk,
+  estimate_high_risk_cases, L95_high_risk_cases, U95_high_risk_cases,
+  estimate_events_prevented, L95_events_prevented, U95_events_prevented,
+  estimate_NNS, L95_NNS, U95_NNS,
+  estimate_NNT, L95_NNT, U95_NNT
+)]
+fwrite(dt2, sep="\t", quote=FALSE, file="analyses/public_health_modelling/targeted_screening_supp.txt")
+
+dt3 <- dcast(targeted, sex + people + cases + model ~ number, value.var=c("estimate", "L95", "U95"))
+dt3[, sex := factor(sex, levels=c("Males", "Females"))]
+dt3[, model := factor(model, levels=c("SCORE2", "SCORE2 + NMR scores", "SCORE2 + PRSs", "SCORE2 + NMR scores + PRSs"))]
+dt3 <- dt3[order(model)][order(sex)]
+dt3 <- dt3[,.(sex,
+  estimate_medium_risk_score2, L95_medium_risk_score2, U95_medium_risk_score2,
+  estimate_medium_risk_score2_cases, L95_medium_risk_score2_cases, U95_medium_risk_score2_cases,
+  model, 
+  estimate_reclassified, L95_reclassified, U95_reclassified,
+  estimate_reclassified_cases, L95_reclassified_cases, U95_reclassified_cases,
+  estimate_delta_events_prevented, L95_delta_events_prevented, U95_delta_events_prevented
+)]
+fwrite(dt3, sep="\t", quote=FALSE, file="analyses/public_health_modelling/targeted_screening2_supp.txt")  
+
+# Build supp tables split into each age group
+dt1 <- fread("analyses/public_health_modelling/blanket_screening/population_screening_by_sex_and_age_group.txt")
+dt2 <- fread("analyses/public_health_modelling/targeted_screening/population_screening_by_sex_and_age_group.txt")
+dt2[, model := paste("SCORE2 then", model)]
+dt <- rbind(idcol="strategy", "population"=dt1, "targeted"=dt2)
+dt[, sex := factor(sex, levels=c("Males", "Females"))]
+
+dt1 <- dcast(dt[number == "high_risk"], sex + age_group + people ~ model, value.var=c("estimate", "L95", "U95"))
+dt1 <- dt1[,.(sex, age_group, people, 
+  estimate_SCORE2, L95_SCORE2, U95_SCORE2,
+  `estimate_SCORE2 + NMR scores`, `L95_SCORE2 + NMR scores`, `U95_SCORE2 + NMR scores`,
+  `estimate_SCORE2 + PRSs`, `L95_SCORE2 + PRSs`, `U95_SCORE2 + PRSs`,
+  `estimate_SCORE2 + NMR scores + PRSs`, `L95_SCORE2 + NMR scores + PRSs`, `U95_SCORE2 + NMR scores + PRSs`
+)]
+fwrite(dt1, sep="\t", quote=FALSE, file="analyses/public_health_modelling/high_risk_by_sex_and_age_for_supp.txt")
+
+dt2 <- dcast(dt[number == "high_risk_cases"], sex + age_group + cases ~ model, value.var=c("estimate", "L95", "U95"))
+dt2 <- dt2[,.(sex, age_group, cases, 
+  estimate_SCORE2, L95_SCORE2, U95_SCORE2,
+  `estimate_SCORE2 + NMR scores`, `L95_SCORE2 + NMR scores`, `U95_SCORE2 + NMR scores`,
+  `estimate_SCORE2 + PRSs`, `L95_SCORE2 + PRSs`, `U95_SCORE2 + PRSs`,
+  `estimate_SCORE2 + NMR scores + PRSs`, `L95_SCORE2 + NMR scores + PRSs`, `U95_SCORE2 + NMR scores + PRSs`
+)]
+fwrite(dt2, sep="\t", quote=FALSE, file="analyses/public_health_modelling/high_risk_cases_by_sex_and_age_for_supp.txt")
+
+dt3 <- dcast(dt[number == "medium_risk_score2" & model == "SCORE2 then SCORE2 + NMR scores"], age_group ~ sex, value.var=c("estimate", "L95", "U95")) 
+dt3 <- dt3[, .(age_group, estimate_Males, L95_Males, U95_Males, estimate_Females, L95_Females, U95_Females)]
+fwrite(dt3, sep="\t", quote=FALSE, file="analyses/public_health_modelling/intermediate_risk_score2_by_sex_and_age_for_supp.txt")
+
+dt4 <- dcast(dt[number == "medium_risk_score2_cases" & model == "SCORE2 then SCORE2 + NMR scores"], age_group ~ sex, value.var=c("estimate", "L95", "U95")) 
+dt4 <- dt4[, .(age_group, estimate_Males, L95_Males, U95_Males, estimate_Females, L95_Females, U95_Females)]
+fwrite(dt4, sep="\t", quote=FALSE, file="analyses/public_health_modelling/intermediate_risk_score2_cases_by_sex_and_age_for_supp.txt")
+
+dt5 <- dcast(dt[number == "reclassified"], sex + age_group ~ model, value.var=c("estimate", "L95", "U95"))
+dt5 <- dt5[,.(sex, age_group, 
+  `estimate_SCORE2 then SCORE2 + NMR scores`, `L95_SCORE2 then SCORE2 + NMR scores`, `U95_SCORE2 then SCORE2 + NMR scores`,
+  `estimate_SCORE2 then SCORE2 + PRSs`, `L95_SCORE2 then SCORE2 + PRSs`, `U95_SCORE2 then SCORE2 + PRSs`,
+  `estimate_SCORE2 then SCORE2 + NMR scores + PRSs`, `L95_SCORE2 then SCORE2 + NMR scores + PRSs`, `U95_SCORE2 then SCORE2 + NMR scores + PRSs`
+)]
+fwrite(dt5, sep="\t", quote=FALSE, file="analyses/public_health_modelling/reclassified_by_sex_and_age_for_supp.txt")
+
+dt6 <- dcast(dt[number == "reclassified_cases"], sex + age_group ~ model, value.var=c("estimate", "L95", "U95"))
+dt6 <- dt6[,.(sex, age_group, 
+  `estimate_SCORE2 then SCORE2 + NMR scores`, `L95_SCORE2 then SCORE2 + NMR scores`, `U95_SCORE2 then SCORE2 + NMR scores`,
+  `estimate_SCORE2 then SCORE2 + PRSs`, `L95_SCORE2 then SCORE2 + PRSs`, `U95_SCORE2 then SCORE2 + PRSs`,
+  `estimate_SCORE2 then SCORE2 + NMR scores + PRSs`, `L95_SCORE2 then SCORE2 + NMR scores + PRSs`, `U95_SCORE2 then SCORE2 + NMR scores + PRSs`
+)]
+fwrite(dt6, sep="\t", quote=FALSE, file="analyses/public_health_modelling/reclassified_cases_by_sex_and_age_for_supp.txt")
+
+
+
+
+
+
+
+
