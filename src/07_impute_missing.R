7711301ecf8aca01954b99dff021f79251c5a8d5
Scott Ritchie <sritchie73@gmail.com>
2023-10-10 11:01:14 +0100

    Imputation now sets K based on PCs

--- src/07_impute_missing.R
+++ src/07_impute_missing.R
@@ -11,14 +11,26 @@ dat[is.na(smoking), smoking := FALSE]
 
 # extract the non-derived nmr biomarkers for which we'll impute missing data
 nmr_info <- fread("data/ukb/NMR_metabolomics/biomarker_information.txt")
-nmr_dat <- dat[,.SD,.SDcols=c("eid", nmr_info[Type == "Non-derived", Biomarker])]
+non_derived <- nmr_info[Type == "Non-derived", Biomarker]
+non_derived <- setdiff(non_derived, "Clinical_LDL_C")
+nmr_dat <- dat[,.SD,.SDcols=c("eid", non_derived)]
 
 # Drop all nmr biomarkers (ratios included) from the main dataset
 dat[, c(nmr_info[,Biomarker]) := NULL]
 
-# Impute the missing data using K-nearest neighbours
+# Convert to matrix
 nmr_dat <- as.matrix(nmr_dat, rownames="eid")
-nmr_dat <- impute::impute.knn(t(nmr_dat)) # Expects samples as columns
+
+# Do first pass imputation with default KNN so we can run PCA to determine better number for K
+pca_dat <- impute::impute.knn(t(nmr_dat)) # Expects samples as columns
+pcs <- prcomp(t(pca_dat$data), scale=TRUE)
+pc_info <- as.data.table(t(summary(pcs)$importance), keep.rownames="PC")
+setnames(pc_info, c("PC", "SD", "Prop.Var", "Cumul.Prop.Var"))
+fwrite(pc_info, sep="\t", quote=FALSE, file="data/imputed/pca.txt")
+
+# Set K to number of PCs explaining >95% of the variation (K=20)
+K <- pc_info[,which(Cumul.Prop.Var > 0.95)[1]]
+nmr_dat <- impute::impute.knn(t(nmr_dat), k=20)
 nmr_dat <- t(nmr_dat$data)
 nmr_dat <- as.data.table(nmr_dat, keep.rownames="eid")
 nmr_dat <- ukbnmr::recompute_derived_biomarkers(nmr_dat)
