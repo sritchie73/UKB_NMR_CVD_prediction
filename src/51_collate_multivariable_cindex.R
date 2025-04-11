be59904f46b2ce0714dc12e5da2338f2b9c68c13
Scott Ritchie <sritchie73@gmail.com>
2025-04-11 14:06:37 +0100

    Fig 3A

--- src/51_collate_multivariable_cindex.R
+++ src/51_collate_multivariable_cindex.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(foreach)
+library(forcats)
 library(ggplot2)
 library(ggstance)
 library(patchwork)
@@ -10,6 +11,37 @@ res <- rbind(idcol="cohort",
   "replication"=fread("analyses/test/replication_cindices.txt"),
   "pooled"=fread("analyses/test/pooled_cindices.txt")
 )
+
+# Build main figure 3A
+ggdt <- res[cohort == "pooled" & model_sex == "Sex-stratified" & endpoint == "cvd" & score == "SCORE2_excl_UKB"]
+ggdt[, model_name := fcase(
+  model == "SCORE2 + NMR scores", "SCORE2 + NMR scores for CHD and IS",
+  model == "SCORE2 + Biochemistry", "SCORE2 + 11 clinical chemistry biomarkers",
+  model == "SCORE2 + PRS", "SCORE2 + PRSs for CHD and IS",
+  model == "SCORE2 + NMR scores + PRS", "SCORE2 + NMR scores for CHD and IS + PRSs for CHD and IS",
+  model == "SCORE2 + Biochemistry + PRS", "SCORE2 + 11 clinical chemistry biomarkers + PRSs for CHD and IS"
+)]
+ggdt[,model_name := factor(model_name, levels=model_name)]
+
+g <- ggplot(ggdt) +
+  aes(x=deltaC, xmin=deltaC.L95, xmax=deltaC.U95, y=fct_rev(model_name), color=model) +
+  geom_vline(xintercept=0, linetype=2) +
+  geom_errorbarh(height=0) +
+  geom_point(shape=23, size=2, fill="white") +
+  scale_x_continuous("Change in C-index (95% CI)", limits=c(0, 0.03), breaks=c(0, 0.01, 0.02, 0.03)) +
+	scale_color_manual(values=c(
+		"SCORE2 + NMR scores"="#e41a1c", "SCORE2 + PRS"="#377eb8", "SCORE2 + Biochemistry"="#ff7f00",
+		"SCORE2 + NMR scores + PRS"="#4daf4a", "SCORE2 + Biochemistry + PRS"="#984ea3"
+	)) +
+  ylab("") +
+  theme_bw() +
+  theme(
+    axis.text.y=element_text(size=8, color="black"), axis.title.y=element_blank(),
+    axis.text.x=element_text(size=6), axis.title.x=element_text(size=8),
+    panel.grid.major.y=element_blank(), panel.grid.minor.y=element_blank(),
+    legend.position="none"
+  )
+ggsave(g, width=5.3, height=2, file="analyses/test/main_delta_cindices.pdf")
 
 # Build supp table of main results
 supp1 <- res[endpoint == "cvd" & model_sex == "Sex-stratified" & score == "SCORE2_excl_UKB"]
